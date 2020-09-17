f67e3d4652a1d827daa7ac11951f971f01682189
Ariel Ibarra <arielibarra39@gmail.com>
2020-09-17 20:08:58 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,23 +1,9 @@
 class School 
   
-  attr_reader :name, :roster, :add_student, :length
+  attr_reader :name
   
-  def initialize(name, roster)
+  def initialize(name)
     @name = name
-    @roster = roster.length
   end 
-  
-  def roster=(roster)
-    @roster = roster
-  end 
-  
-  def length=(length)
-    @length = length
-    legnth = []
-  end 
-  
-  def add_student=(student)
-    @add_student = student 
-  end 
-  
 end 
+  
