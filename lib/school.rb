a6ea34ef4331b8ae2dab4352eadc61b643abcc51
Ariel Ibarra <arielibarra39@gmail.com>
2020-09-17 19:30:46 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,11 +1,12 @@
 class School 
   
+  attr_accessor :roster, :add_student 
+  
+  attr_reader :name 
+  
   def initialize(name)
     @name = name
   end 
   
-  def name  
-    @name 
-  end 
   
 end 
